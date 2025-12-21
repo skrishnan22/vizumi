ea1b81b9cd1e241f8686e14b3ad2a6628120cc43
S.Krishnan <skrish22195@gmail.com>
2025-12-21 17:35:22 +0530

    dont show loading for diagram

--- src/components/DiagramRenderer.tsx
+++ src/components/DiagramRenderer.tsx
@@ -61,21 +61,6 @@ async function fetchDiagramSvg(diagramCode: string, headers: HeadersInit) {
   return data.svg;
 }
 
-function StatusMessage({ message }: { message: string }) {
-  return (
-    <p
-      style={{
-        fontSize: '0.9rem',
-        textAlign: 'center',
-        margin: 0,
-        color: 'rgba(28, 26, 23, 0.8)',
-      }}
-    >
-      {message}
-    </p>
-  );
-}
-
 export function DiagramRenderer({
   code,
   cachedSvg,
@@ -188,11 +173,7 @@ export function DiagramRenderer({
   }
 
   if (isLoading || !parsedSvg) {
-    return (
-      <div className={className}>
-        <StatusMessage message="Sketching diagram..." />
-      </div>
-    );
+    return null;
   }
 
   return <div className={className}>{parsedSvg}</div>;
